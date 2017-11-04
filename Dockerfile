0cb1a06bd1f6fc496d23e19021cbd2c11b24462c
stpork <stpork@gmail.com>
2017-11-04 23:50:40 +0200

    Some renaming

--- Dockerfile
+++ Dockerfile
@@ -17,12 +17,12 @@ RUN BAMBOO_URL=http://www.atlassian.com/software/bamboo/downloads/binary/atlassi
 && M2_URL=https://bitbucket.org/stpork/bamboo-agent/downloads/settings.xml \
 && mkdir -p ${BAMBOO_INSTALL} \
 && mkdir -p ${BAMBOO_HOME} \
-&& curl -L --silent ${BAMBOO_URL} | tar -xz --strip-components=1 -C "$BAMBOO_INSTALL" \
+&& curl -fsSL ${BAMBOO_URL} | tar -xz --strip-components=1 -C "$BAMBOO_INSTALL" \
 && echo -e "\nbamboo.home=$BAMBOO_HOME" >> "${BAMBOO_INSTALL}/atlassian-bamboo/WEB-INF/classes/bamboo-init.properties" \
-&& chown -R ${BAMBOO_USER}:${BAMBOO_GROUP} ${BAMBOO_INSTALL} \
+&& chown -R ${RUN_USER}:${RUN_GROUP} ${BAMBOO_INSTALL} \
 && chmod -R 777 ${BAMBOO_INSTALL}
 
-USER ${BAMBOO_USER}:${BAMBOO_GROUP}
+USER ${RUN_USER}:${RUN_GROUP}
 
 EXPOSE 8085
 EXPOSE 54663
--- entrypoint.sh
+++ entrypoint.sh
@@ -28,17 +28,17 @@ ARGS="$@"
 
 # Start Bamboo as the correct user.
 if [ "${UID}" -eq 0 ]; then
-    echo "User is currently root. Will change directory ownership to ${BAMBOO_USER}:${BAMBOO_GROUP}, then downgrade permission to ${BAMBOO_USER}"
+    echo "User is currently root. Will change directory ownership to ${RUN_USER}:${RUN_GROUP}, then downgrade permission to ${RUN_USER}"
     PERMISSIONS_SIGNATURE=$(stat -c "%u:%U:%a" "${BAMBOO_HOME}")
-    EXPECTED_PERMISSIONS=$(id -u ${BAMBOO_USER}):${BAMBOO_USER}:700
+    EXPECTED_PERMISSIONS=$(id -u ${RUN_USER}):${RUN_USER}:700
     if [ "${PERMISSIONS_SIGNATURE}" != "${EXPECTED_PERMISSIONS}" ]; then
         echo "Updating permissions for BAMBOO_HOME"
         mkdir -p "${BAMBOO_HOME}/lib" &&
             chmod -R 700 "${BAMBOO_HOME}" &&
-            chown -R "${BAMBOO_USER}:${BAMBOO_GROUP}" "${BAMBOO_HOME}"
+            chown -R "${RUN_USER}:${BAMBOO_GROUP}" "${BAMBOO_HOME}"
     fi
     # Now drop privileges
-    exec su -s /bin/bash "${BAMBOO_USER}" -c "${BAMBOO_INSTALL}/bin/start-bamboo.sh ${ARGS}"
+    exec su -s /bin/bash "${RUN_USER}" -c "${BAMBOO_INSTALL}/bin/start-bamboo.sh ${ARGS}"
 else
     exec ${BAMBOO_INSTALL}/bin/start-bamboo.sh ${ARGS}
 fi
